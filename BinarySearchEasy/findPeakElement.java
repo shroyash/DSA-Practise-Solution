e801c157ff579a4f666d4de27af78813453bd99f
shroyash <shroyash35@gmail.com>
2025-01-11 21:18:16 +0545

    dsa soln updated

---
+++ BinarySearchEasy/findPeakElement.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int start = 1;
+        int end = nums.length-2;
+        if(nums.length ==1){
+            return 0;
+        }
+        if(nums[0] > nums[1]){
+            return 0;
+        }
+        if(nums[nums.length-1] > nums[nums.length-2]){
+            return nums.length-1;
+        }
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]){
+                return mid;
+            }else if(nums[mid]<nums[mid+1]){
+                start = mid + 1 ;
+            }else{
+                end = end-1;
+            }
+    
+        }
+        return -1;
+        
+    }
+}
+        
+   
